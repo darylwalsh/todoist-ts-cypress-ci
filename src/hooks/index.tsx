ea9e68120c3d3805b38d184d8a1fb909f5087285
Daryl <daryl@blpc.us>
2019-08-30 02:21:17 -0400

    feat(typescript): complete typescript typings of legacy base
    added typescript to legacy js base in preparation for updating code

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,25 +1,43 @@
+import { KeyPairKeyObjectResult } from 'crypto'
+import * as firebase from 'firebase/app'
 import moment from 'moment'
-import React, { useEffect, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 
-import { firebase } from '../firebase'
-import { collatedTasksExists } from '../helpers'
+import { QueryDocumentSnapshot } from '@google-cloud/firestore'
+
+//import { firebase } from '../firebase'
+import { collatedTasksExist } from '../helpers'
 
 interface ProjectInterface {
   name: string
-  projectid: string
-  userid: string
+  projectid?: string
+  userid?: string
+  next?: any
+  error?: any
+  complete?: any
+}
+
+interface TasksInterface {
+  archived?: boolean
+  id?: string
+  projectId?: string
+  taskId?: string
+  taskdate?: string
+  taskdesc?: string
+  taskname?: string
+  userId?: string
 }
 export const useTasks = (selectedProject: string) => {
   const [tasks, setTasks] = useState([])
   const [archivedTasks, setArchivedTasks] = useState([])
   useEffect(() => {
-    let unsubscribe = firebase
+    let unsubscribe: Partial<firebase.firestore.Query | any> = firebase
       .firestore()
       .collection('tasks')
       .where('userId', '==', '3XueJHiEDvXqB9PLmuM6AkRZExD3')
 
     unsubscribe =
-      selectedProject && !collatedTasksExists(selectedProject)
+      selectedProject && !collatedTasksExist(selectedProject)
         ? (unsubscribe = unsubscribe.where('projectid', '==', selectedProject))
         : selectedProject === 'TODAY'
         ? (unsubscribe = unsubscribe.where(
@@ -30,25 +48,31 @@ export const useTasks = (selectedProject: string) => {
         : selectedProject === 'INBOX' || selectedProject === '0'
         ? (unsubscribe = unsubscribe.where('date', '==', ''))
         : unsubscribe
-    unsubscribe = unsubscribe.onSnapshot(snapshot => {
-      const newTasks = snapshot.docs.map(task => ({
-        id: task.id,
-        ...task.data(),
-      }))
+    unsubscribe = unsubscribe.onSnapshot(
+      (snapshot: Partial<firebase.firestore.QuerySnapshot> | any) => {
+        const newTasks = snapshot.docs.map(
+          (task: firebase.firestore.QueryDocumentSnapshot) => ({
+            id: task.id,
+            ...task.data(),
+          })
+        )
 
-      setTasks(
-        selectedProject === 'NEXT_7'
-          ? newTasks.filter(
-              task =>
-                moment(task.date, 'DD-MM-YYYY').diff(moment(), 'days') <= 7 &&
-                task.archived !== true
-            )
-          : newTasks.filter(task => task.archived !== true)
-      )
+        setTasks(
+          selectedProject === 'NEXT_7'
+            ? newTasks.filter(
+                (task: TasksInterface) =>
+                  moment(task.taskdate, 'DD-MM-YYYY').diff(moment(), 'days') <=
+                    7 && task.archived !== true
+              )
+            : newTasks.filter((task: TasksInterface) => task.archived !== true)
+        )
 
-      setArchivedTasks(newTasks.filter(task => task.archived !== false))
-    })
-    return () => unsubscribe()
+        setArchivedTasks(
+          newTasks.filter((task: TasksInterface) => task.archived !== false)
+        )
+      }
+    )
+    return () => unsubscribe
   }, [selectedProject])
 
   return { tasks, archivedTasks }
